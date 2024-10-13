58f9b6964c85341cd0215dbab5df7bd70c10df90
Pietu R <pietu_r@gmx.com>
2024-10-13 13:36:48 +0300

    add utils module

---
+++ src/cli/utils.rs
@@ -0,0 +1,34 @@
+use std::{
+    collections::HashSet,
+    path::{Path, PathBuf},
+};
+
+use walkdir::DirEntry;
+
+use super::types::AppError;
+
+pub fn is_visible(entry: &DirEntry) -> bool {
+    entry
+        .file_name()
+        .to_str()
+        .map(|s| !s.starts_with("."))
+        .unwrap_or(true)
+}
+
+pub fn is_file(entry: &DirEntry) -> bool {
+    entry.path().is_file()
+}
+
+pub fn parents<'a, I>(paths: I) -> Result<HashSet<PathBuf>, AppError>
+where
+    I: Iterator<Item = PathBuf>,
+{
+    paths
+        .map(PathBuf::from)
+        .map(|p| match p.parent() {
+            Some(parent) if parent == Path::new("") => Ok(p),
+            Some(parent) => Ok(parent.to_path_buf()),
+            None => Err(AppError::PathError(p)),
+        })
+        .collect()
+}
